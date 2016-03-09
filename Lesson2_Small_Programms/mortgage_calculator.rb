b9e88ae04bc015f39296890fe42207d4a1314f28
Mohammed Ali <mdeeq.ali@gmail.com>
2016-03-09 14:07:27 +0000

    Completed mortgage calculator

--- Lesson2_Small_Programms/mortgage_calculator.rb
+++ Lesson2_Small_Programms/mortgage_calculator.rb
@@ -8,37 +8,88 @@ write mortgage calculator that calculates the following two thngs:
 =end
 
 def prompt(message)
-  Kernel.puts("=> #{message}")
+  puts("=> #{message}")
 end
 
-prompt("Enter loan amount you wish to borrow pleaase:")
-loan_amount = gets.chomp.to_i
+loop do
+  prompt("Welcome to Mortgage calculator")
+  prompt("-------------------------------")
 
-prompt("Enter the annual interst rate(APR) in digits please")
-annual_interest = gets.chomp.to_f/100
+  name = ''
+  prompt("What's your name?")
+  name = gets.chomp
 
-monthly_interest = (annual_interest / 12)
+  loop do
+    if name.empty?
+      puts "Hmmm... Don't you have name?"
+      prompt("Enter your name!")
+      name = gets.chomp
+    else
+      break
+    end
+  end
 
-prompt("Enter loan duration, in years please:")
-duration_years = gets.chomp.to_i
+prompt("Hi #{name}, What's the loan amount?")
 
-# calculating total number of paymnets over the loan period
-# 12 months in year times by number of laon years
-duration_months = (duration_years * 12)
+loan_amount = ''
 
+  # Checking if user entered correct amount and repeating until it's entered valid numbers.
+  loop do
+    loan_amount = gets.chomp
+    if loan_amount.empty? || loan_amount.to_f < 0
+      prompt("Must enter positive number.")
+    else
+      break
+    end
+  end
 
-# Mortgage formula to work out the sum
-top = monthly_interest * ((1 + monthly_interest) ** duration_months) 
-bottom = ((1 + monthly_interest) ** duration_months -1)
-monthly_payments = (loan_amount * (top / bottom)).round(2)
+  prompt("What is the annnual interst rate(APR)?")
+  prompt("Example: 5 for 5% or 2.5 for 2.5%")
 
+  interest_rate = ''
 
-puts "----------------------------------------------"
-prompt("Your montly payment is: #{monthly_payments}")
-puts "----------------------------------------------"
-prompt("In #{duration_months} months you would repay the full loan")
-puts "----------------------------------------------"
+  loop do
+    interest_rate = gets.chomp
 
+    if interest_rate.empty? || interest_rate.to_f < 0
+      prompt("Must enter positive number")
+    else
+      break
+    end
+  end
 
+  prompt("What is the loan duration(in years)")
 
+  loan_years = ''
+  loop do
+    loan_years = gets.chomp
 
+    if loan_years.empty? || loan_years.to_i < 0
+      prompt("Enter valid number")
+    else
+      break
+    end
+  end
+
+  annual_interest_rate = interest_rate.to_f / 100
+  monthly_interest_rate = (annual_interest_rate / 12)
+  loan_months = (loan_years.to_i * 12)
+
+  # Mortgage calculator formula to working out the values
+  monthly_payments = \
+    loan_amount.to_f * (monthly_interest_rate * (1 + monthly_interest_rate)**loan_months) /
+                       ((1 + monthly_interest_rate)**loan_months - 1)
+
+  puts "----------------------------------------------"
+  prompt("Your montly payment is: £#{monthly_payments.round(2)}")
+  puts "----------------------------------------------"
+  prompt("It will take you (#{name}) -#{loan_months}- months to repay the loan.")
+  puts "----------------------------------------------"
+
+  prompt("Do you want to perform another calculation?(Y to continue")
+  answer = gets.chomp.downcase
+  break unless answer.start_with?("y")
+end
+
+prompt("Thank you for using the Mortgage Calculator :-)")
+prompt("Come back soon.")
