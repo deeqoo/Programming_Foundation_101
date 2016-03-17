0f7d72d95d9b87fa8bbe53109f42ce900ff419eb
Mohammed Ali <mdeeq.ali@gmail.com>
2016-03-17 18:05:22 +0000

    Adding RPS features to rock paper game

--- Lesson2_Small_Programms/rock_paper_scissors.rb
+++ Lesson2_Small_Programms/rock_paper_scissors.rb
@@ -1,16 +1,20 @@
 # Rock Paper Scissors games, Lesson 2, 101 Prog Foundation
 
 # List of valid options for the game
-VALID_CHOICES = %w(rock paper scissors)
+VALID_CHOICES = %w(r p sc l sp).freeze()
 
 def prompt(message)
   Kernel.puts("=> #{message}")
 end
 
+prompt("welcome to Rock Paper Scissors Lizar and Spock game")
+
 def win?(first, second)
-  (first == 'rock' && second == 'scissors') ||
-    (first == 'paper' && second == 'rock') ||
-    (first == 'scissors' && second == 'paper')
+  (first == 'r' && %w(sc l).include?(second)) ||
+    (first == 'p' && %w(r sp).include?(second)) ||
+    (first == 'sc' && %w(p l).include?(second)) ||
+    (first == 'l' && %w(sp p).include?(second)) ||
+    (first == 'sp' && %w(sc r).include?(second))
 end
 
 def display_result(player, computer)
@@ -22,6 +26,36 @@ def display_result(player, computer)
     prompt("It's tie!")
   end
 end
+
+player_score = 0
+computer_score = 0
+
+def display_score(player, computer)
+  prompt("Your score is: #{player_score}")
+  prompt("Computer score is: #{computer_score}")
+end
+
+# def winner(player, computer)
+#   if player >= 5
+#     prompt("You won #{player_score} times")
+#   elsif computer >= 5
+#     prompt("Sorry you lost, computer won #{computer_score} times")
+#   end
+# end
+
+display_terms = <<-MSG
+
+  Key terms for the games.
+  --------------------------------------
+  Select r for Rock
+  Select p for Paper
+  Select sc for Scissors
+  Select l for Lizard
+  Select sp for Spock
+  --------------------------------------
+MSG
+
+prompt(display_terms)
 
 loop do # Loop for choice validation.
   choice = ''
@@ -35,9 +69,18 @@ loop do # Loop for choice validation.
 
   computer_choice = VALID_CHOICES.sample
 
+  Kernel.puts("You choose: #{choice}; Computer Chooose: #{computer_choice}")
+  
   display_result(choice, computer_choice)
-  Kernel.puts("You choose: #{choice}; Computer chose: #{computer_choice}")
+  
+  if win?(choice, computer_choice)
+    player_score += 1
+  elsif win?(computer_choice, choice)
+    computer_score += 1
+  end
 
+  display_score(player_score, computer_score)
+  
   prompt("Do you want to play agian?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?("y")
