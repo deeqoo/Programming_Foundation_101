2e3af141530c783a958271928b5ae2f0b22d1ef7
Mohammed Ali <mdeeq.ali@gmail.com>
2016-03-19 15:01:26 +0000

    complted RPS features and added few more lines.

--- Lesson2_Small_Programms/rock_paper_scissors.rb
+++ Lesson2_Small_Programms/rock_paper_scissors.rb
@@ -8,6 +8,7 @@ def prompt(message)
 end
 
 prompt("welcome to Rock Paper Scissors Lizar and Spock game")
+prompt("Win 5 times against the computer to win the game.")
 
 def win?(first, second)
   (first == 'r' && %w(sc l).include?(second)) ||
@@ -27,21 +28,10 @@ def display_result(player, computer)
   end
 end
 
-player_score = 0
-computer_score = 0
-
 def display_score(player, computer)
-  prompt("Your score is: #{player_score}")
-  prompt("Computer score is: #{computer_score}")
+  prompt("Your score is: #{player}")
+  prompt("Computer score is: #{computer}")
 end
-
-# def winner(player, computer)
-#   if player >= 5
-#     prompt("You won #{player_score} times")
-#   elsif computer >= 5
-#     prompt("Sorry you lost, computer won #{computer_score} times")
-#   end
-# end
 
 display_terms = <<-MSG
 
@@ -57,32 +47,50 @@ MSG
 
 prompt(display_terms)
 
-loop do # Loop for choice validation.
-  choice = ''
+loop do # Main loop.
+  player_score = 0
+  computer_score = 0
+
   loop do
-    prompt("Choone one: #{VALID_CHOICES.join(' ')}")
-    choice = Kernel.gets().chomp()
+    choice = ''
 
-    break if VALID_CHOICES.include?(choice)
-    prompt("That's not valid choice")
+    loop do # choice validation loop
+      prompt("Choone one: #{VALID_CHOICES.join(' ')}")
+      choice = Kernel.gets().chomp()
+
+      # Exiting the program should player no longer wants to play.
+      if choice == 'exit' || choice == 'end'
+        prompt('Games terminated early, good bye')
+        exit
+      end
+
+      break if VALID_CHOICES.include?(choice)
+      prompt("That's not valid choice")
+    end # end of choice validation loop
+
+    computer_choice = VALID_CHOICES.sample
+
+    Kernel.puts("You choose: #{choice}; Computer Chooose: #{computer_choice}")
+    display_result(choice, computer_choice)
+    if win?(choice, computer_choice)
+      player_score += 1
+    elsif win?(computer_choice, choice)
+      computer_score += 1
+    end
+
+    display_score(player_score, computer_score)
+
+    break if (player_score >= 5) || (computer_score >= 5)
+  end # end of second loop
+
+  if player_score >= 5
+    puts "Congratulatiosn you won the game, your final score is: #{player_score}"
+  elsif computer_score >= 5
+    puts "Sorry, you lost the game and final score is: #{computer_score}"
   end
-
-  computer_choice = VALID_CHOICES.sample
-
-  Kernel.puts("You choose: #{choice}; Computer Chooose: #{computer_choice}")
-  
-  display_result(choice, computer_choice)
-  
-  if win?(choice, computer_choice)
-    player_score += 1
-  elsif win?(computer_choice, choice)
-    computer_score += 1
-  end
-
-  display_score(player_score, computer_score)
-  
   prompt("Do you want to play agian?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?("y")
 end
+
 prompt("Thank you for playing, Good bye!")
