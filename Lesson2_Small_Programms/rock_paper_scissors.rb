836b3d7a8d0f3e8ff06311cc16283b1afeaf692f
Mohammed Ali <mdeeq.ali@gmail.com>
2016-03-21 22:21:51 +0000

    refacotored and cleaned code

--- Lesson2_Small_Programms/rock_paper_scissors.rb
+++ Lesson2_Small_Programms/rock_paper_scissors.rb
@@ -1,6 +1,5 @@
 # Rock Paper Scissors games, Lesson 2, 101 Prog Foundation
 
-# List of valid options for the game
 VALID_CHOICES = %w(r p sc l sp).freeze()
 
 def prompt(message)
@@ -9,28 +8,62 @@ end
 
 prompt("welcome to Rock Paper Scissors Lizar and Spock game")
 prompt("Win 5 times against the computer to win the game.")
+prompt("To exit the program at any time, enter 'exit' or 'end'")
+
+WINNING_CHOICES = {
+  'r' => %w(sc l),
+  'p' => %w(r sp),
+  'sc' => %w(p l),
+  'sp' => %w(sc r),
+  'l' => %w(sp p)
+}.freeze()
+
+def initials_hash
+  {
+    'r' => 'Rock',
+    'p' => "Paper",
+    'sc' => "Scissors",
+    'sp' => "Spock",
+    'l' => "Lizard"
+  }
+end
+
+def initials_conversions(op)
+  initials_hash[op]
+end
 
 def win?(first, second)
-  (first == 'r' && %w(sc l).include?(second)) ||
-    (first == 'p' && %w(r sp).include?(second)) ||
-    (first == 'sc' && %w(p l).include?(second)) ||
-    (first == 'l' && %w(sp p).include?(second)) ||
-    (first == 'sp' && %w(sc r).include?(second))
+  WINNING_CHOICES[first].include?(second)
 end
 
 def display_result(player, computer)
   if win?(player, computer)
-    prompt("You win!")
+    prompt("You beat the computer, 1 point!")
   elsif win?(computer, player)
-    prompt("Computer won!")
+    prompt("Ops, You lost, 1 point for the computer!")
   else
     prompt("It's tie!")
   end
 end
 
+def points_score(player_score, computer_score)
+  if player_score >= 5
+    prompt("Congratulatiosn you won the game :-)")
+    prompt("------------------------------------")
+  elsif computer_score >= 5
+    prompt("Sorry, you lost the game, :-( ")
+    prompt("------------------------------------")
+  end
+end
+
+def total_score(player, computer)
+  prompt("You final score point(s) is/are: #{player}")
+  prompt("Computer final score point(s) is/are: #{computer}")
+end
+
 def display_score(player, computer)
-  prompt("Your score is: #{player}")
-  prompt("Computer score is: #{computer}")
+  prompt("You have: #{player} point(s)")
+  prompt("Computer has: #{computer} point(s)")
 end
 
 display_terms = <<-MSG
@@ -54,24 +87,24 @@ loop do # Main loop.
   loop do
     choice = ''
 
-    loop do # choice validation loop
+    loop do
       prompt("Choone one: #{VALID_CHOICES.join(' ')}")
       choice = Kernel.gets().chomp()
 
-      # Exiting the program should player no longer wants to play.
-      if choice == 'exit' || choice == 'end'
+      if %w(exit end).include?(choice)
         prompt('Games terminated early, good bye')
         exit
       end
 
       break if VALID_CHOICES.include?(choice)
       prompt("That's not valid choice")
-    end # end of choice validation loop
+    end
 
     computer_choice = VALID_CHOICES.sample
 
-    Kernel.puts("You choose: #{choice}; Computer Chooose: #{computer_choice}")
+    Kernel.puts("You choose: #{initials_conversions(choice)}; Computer Chooose: #{initials_conversions(computer_choice)}")
     display_result(choice, computer_choice)
+
     if win?(choice, computer_choice)
       player_score += 1
     elsif win?(computer_choice, choice)
@@ -81,13 +114,14 @@ loop do # Main loop.
     display_score(player_score, computer_score)
 
     break if (player_score >= 5) || (computer_score >= 5)
-  end # end of second loop
-
-  if player_score >= 5
-    puts "Congratulatiosn you won the game, your final score is: #{player_score}"
-  elsif computer_score >= 5
-    puts "Sorry, you lost the game and final score is: #{computer_score}"
   end
+
+  points_score(player_score, computer_score)
+  prompt("------ Final score of the game ------")
+  prompt("------------------------------------")
+  total_score(player_score, computer_score)
+  prompt("------------------------------------")
+
   prompt("Do you want to play agian?")
   answer = Kernel.gets().chomp()
   break unless answer.downcase().start_with?("y")
